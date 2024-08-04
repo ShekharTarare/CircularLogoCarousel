7a1be9c0cdcaa070baf857b775c5c09385ea9658
Shekhar Tarare <shekhar.tarare@outlook.com>
2024-08-04 22:04:01 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 5, // Adjust number of visible slides
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1008,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 420,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
